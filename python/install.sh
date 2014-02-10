1c4815256e807b638776a79a9df8c78102c94670
Kyle Kelley <kyle.kelley@rackspace.com>
2014-02-10 21:33:30 +0000

    Install virtualenv and virtualenvwrapper

--- python/install.sh
+++ python/install.sh
@@ -2,3 +2,7 @@
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python2.7
 # pip
 curl --show-error --retry 5 https://raw.github.com/pypa/pip/master/contrib/get-pip.py | sudo python2.7
+
+# virtualenv
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
